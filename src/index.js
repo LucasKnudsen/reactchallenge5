415c7d9073e9d1498d8906799952f157fc561120
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-07 11:48:18 +0100

    Build functionality for simple calculator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  const [input1, setInput1] = useState(0)
+  const [input2, setInput2] = useState(0)
+  const [result, setResult] = useState(0)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const addHandler = () => {
+    setResult(Number(input1) + Number(input2))
+  }
+  const subtractHandler = () => {
+    setResult(Number(input1) - Number(input2))
+  }
+  const multiplyHandler = () => {
+    setResult(Number(input1) * Number(input2))
+  }
+  const divideHandler = () => {
+    setResult(Number(input1) / Number(input2))
+  }
+
+  return (
+    <div className="App">
+      <h1>Adding Two Numbers</h1>
+
+      <div className="number-inputs">
+        <input key={1} type="number" placeholder="0" onChange={(event) => setInput1(event.target.value)} />
+        <input key={2} type="number" placeholder="0" onChange={(event) => setInput2(event.target.value)} />
+      </div>
+
+      <button onClick={() => addHandler()}>Add Them!</button>
+      <button onClick={() => subtractHandler()}>Subtract Them!</button>
+      <button onClick={() => multiplyHandler()}>Multiply Them!</button>
+      <button onClick={() => divideHandler()}>Divide Them!</button>
+
+      <h2>{result}</h2>
+
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
